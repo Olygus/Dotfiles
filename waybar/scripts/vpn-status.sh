e7bcbb02ca7015d51f54170c662e29cd621a8f78
Olygus <farhan.alshahaf@education.nsw.gov.au>
2026-08-07 22:01:21 +1000

    edited how vpn look on waybar

--- waybar/scripts/vpn-status.sh
+++ waybar/scripts/vpn-status.sh
@@ -3,8 +3,8 @@
 # Check if the OpenVPN network interface (tun0) is active
 if ip link show tun0 >/dev/null 2>&1; then
     # VPN is connected
-    echo '{"text": "connected", "class": "connected", "tooltip": "ProtonVPN Connected"}'
+    echo '{"text": "VPN ", "class": "connected", "tooltip": "ProtonVPN Connected"}'
 else
     # VPN is disconnected
-    echo '{"text": "not connected", "class": "disconnected", "tooltip": "VPN Disconnected"}'
+    echo '{"text": "VPN ", "class": "disconnected", "tooltip": "VPN Disconnected"}'
 fi
