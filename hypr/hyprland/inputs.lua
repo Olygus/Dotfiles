99855e47ce1c978ea667940ff38430425bd3e7f2
Olygus <farhan.alshahaf@education.nsw.gov.au>
2026-08-09 18:11:40 +1000

    added more gestures and updates animations

--- hypr/hyprland/inputs.lua
+++ hypr/hyprland/inputs.lua
@@ -1,26 +1,40 @@
 return function()
-    hl.config({
-        input = {
-            kb_layout = "us",
-            kb_variant = "",
-            follow_mouse = 1,
-            touchpad = {
-                natural_scroll = false,
-                middle_button_emulation = false,
-            },
-            accel_profile = "flat",
-            sensitivity = 0,
-        },
-        gestures = {
-            workspace_swipe_distance = 250,
-            workspace_swipe_cancel_ratio = 0.4,
-            workspace_swipe_direction_lock = true,
-        },
-    })
+	hl.config({
+		input = {
+			kb_layout = "us",
+			kb_variant = "",
+			follow_mouse = 1,
+			touchpad = {
+				natural_scroll = false,
+				middle_button_emulation = false,
+			},
+			accel_profile = "flat",
+			sensitivity = 0,
+		},
+		gestures = {
+			workspace_swipe_distance = 180,
+			workspace_swipe_cancel_ratio = 0.1,
+			workspace_swipe_direction_lock = false,
+		},
+	})
 
-    hl.gesture({
-        fingers = 3,
-        direction = "horizontal",
-        action = "workspace",
-    })
+	hl.gesture({
+		fingers = 3,
+		direction = "horizontal",
+		action = "workspace",
+	})
+
+	hl.gesture({
+		fingers = 3,
+		direction = "up",
+		action = "special",
+		workspace_name = "scratchpad",
+	})
+
+	hl.gesture({
+		fingers = 3,
+		direction = "down",
+		action = "special",
+		workspace_name = "scratchpad",
+	})
 end
