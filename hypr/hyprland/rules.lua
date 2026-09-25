0707806c22d2f34dde941889e5555726b2e2938b
Olygus <farhan.alshahaf@education.nsw.gov.au>
2026-09-25 14:10:46 +1000

    Refactor window rules for Steam, Obsidian, and system utilities

--- hypr/hyprland/rules.lua
+++ hypr/hyprland/rules.lua
@@ -22,6 +22,7 @@ return function()
 			class = "^(vlc)$",
 		},
 		float = true,
+		center = true,
 	})
 
 	hl.window_rule({
@@ -93,6 +94,112 @@ return function()
 			class = "org.gnome.SimpleScan",
 		},
 		float = true,
+		center = true,
+	})
+
+	hl.window_rule({
+		name = "thunar-float",
+		match = {
+			class = "^(thunar)$",
+		},
+		float = true,
+		size = "960 600",
+		center = true,
+	})
+
+	hl.window_rule({
+		name = "blueman-float",
+		match = {
+			class = "^(blueman-manager)$",
+		},
+		float = true,
+		size = "700 500",
+		center = true,
+	})
+
+	hl.window_rule({
+		name = "volume-control-float",
+		match = {
+			class = "org.pulseaudio.pavucontrol",
+		},
+		float = true,
+		size = "750 600",
+		center = true,
+	})
+	hl.window_rule({
+		name = "obsidian-settings-float",
+		match = { class = ".*obsidian.*", title = ".*Settings.*" },
+		float = true,
+		size = "950 650",
+		center = true,
+	})
+
+	hl.window_rule({
+		name = "obsidian-recovery-float",
+		match = { class = ".*obsidian.*", title = ".*Recovery.*" },
+		float = true,
+		size = "950 650",
+		center = true,
+	})
+
+	hl.window_rule({
+		name = "obsidian-plugins-float",
+		match = { class = ".*obsidian.*", title = ".*Plugins.*" },
+		float = true,
+		size = "950 650",
+		center = true,
+	})
+
+	hl.window_rule({
+		name = "steam-main-size",
+		match = { class = ".*steam.*", title = ".*Steam.*" },
+		size = "70% 100%",
+	})
+
+	hl.window_rule({
+		name = "steam-friends-size",
+		match = { class = ".*steam.*", title = ".*Friends.*" },
+		float = true,
+		size = "30% 100%",
+	})
+
+	hl.window_rule({
+		name = "steam-settings-float",
+		match = { class = ".*steam.*", title = ".*Settings.*" },
+		float = true,
+		size = "900 650",
+		center = true,
+	})
+
+	hl.window_rule({
+		name = "steam-updater-float",
+		match = { class = ".*steam.*", title = ".*Updater.*" },
+		float = true,
+		size = "900 650",
+		center = true,
+	})
+
+	hl.window_rule({
+		name = "steam-screenshot-float",
+		match = { class = ".*steam.*", title = ".*Screenshot.*" },
+		float = true,
+		size = "900 650",
+		center = true,
+	})
+
+	hl.window_rule({
+		name = "steam-chat-float",
+		match = { class = ".*steam.*", title = ".*Chat.*" },
+		float = true,
+		size = "900 650",
+		center = true,
+	})
+
+	hl.window_rule({
+		name = "steam-nonsteam-float",
+		match = { class = ".*steam.*", title = ".*Add Non%-Steam.*" },
+		float = true,
+		size = "900 650",
 		center = true,
 	})
 end
